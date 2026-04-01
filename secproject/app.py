5c23c180a4ed218e10f64b171df4c281a5a86612
sneha-yadav786 <sneha.7864786@gmail.com>
2026-04-02 00:31:26 +0530

    Flask wtf practise

--- secproject/app.py
+++ secproject/app.py
@@ -1,15 +1,21 @@
-from flask import Flask,render_template,request, redirect, url_for
+from flask import Flask,render_template,request, redirect, url_for,flash
+from forms import RegistrationForm
 app=Flask(__name__)
 app.secret_key="ddfd"
 @app.route('/', methods=['GET','POST'])
 def submit():
-    print(request.method)
-    if request.method=="POST":
-        user=request.form.get('user')
-        feedback=request.form.get('feedback')
-        rating=request.form.get('rating')
-        return render_template("thankyou.html", name=user,  feed=feedback, rating=rating)
-    return render_template("feedback.html")
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        email=form.email.data
+
+        flash("welcome {name}!, You successfully Registered." , "success")
+        return redirect(url_for("success"))
+    return render_template("register.html", form=form)
+@app.route('/success')
+def success():
+    return render_template("success.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ secproject/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email,Length
+
+
+class RegistrationForm(FlaskForm):
+    name=StringField("username", validators=[DataRequired()])
+    email=StringField("Email", validators=[DataRequired(), Email()])
+    password=PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    submit=SubmitField("Register")
